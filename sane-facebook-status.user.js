1fba4f3e750cc5f5249165fef63bd2470f1d58d4
Antony Gelberg <antony.gelberg@gmail.com>
2017-05-04 15:29:09 +0300

    Initial empty commit of Sane Facebook Status

---
+++ sane-facebook-status.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Sane Facebook status
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  render Facebook statuses in a less annoying way
+// @author       Antony Gelberg <antony.gelberg@gmail.com>
+// @match        http://*.facebook.com/*
+// @match        https://*.facebook.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
